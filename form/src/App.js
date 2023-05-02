75bd0bb7e6c3d992dbc0d108588db2945dad5e4d
Miriam C. Okpalaeke <Miriam.okpalaeke@gmail.com>
2023-05-02 02:48:26 +0100

    first commit

---
+++ form/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import './formInput.css';
+import ContactForm from './Components/ContactForm';
+
+function App() {
+  return (
+    <div className="container">
+      <ContactForm/>
+      
+    </div>
+  );
+}
+
+export default App;
